11c2e11ef493d9f44a9b37100e4f0fe942555a48
Maximus Juul <Maximusjjuul@gmail.com>
2022-03-13 14:02:57 +0100

    Update Login.cs

--- IoT Projekt/IoT Projekt/Login.cs
+++ IoT Projekt/IoT Projekt/Login.cs
@@ -14,7 +14,7 @@ namespace IoT_Projekt
         bool CredentialOK = false;
 
         #region Credentials for AccountCheck
-
+        // Here we have the login information for our Account Checker user in the database
         string server = "157.90.11.126";
         string id = "AccountCheck";
         string password = "Losting50##";
@@ -37,23 +37,23 @@ namespace IoT_Projekt
             #endregion
 
             #region accountCheck
-            // Vi skaber en string, som kan bruges til at forbinde til vores database, som en bruger der checker en given tabel igennem for at se om der eksistere en kunde.
+            // We create a string that can be used to connect to our database, as a user who checks a given table through to see if a customer exists.
             string accountCheckConnection = $"Server={server};Port=3306;SslMode=none;User Id={id};Password={password};Database={database}";
 
-            // Vi laver en connection til databasen, med stringen for oven
+            // We make a connection to the database, by using the string above
             connection = new MySqlConnection(accountCheckConnection);
 
             try
             {
-                // Her prøver vi at åbne forbindelsen til databasen. Hvis det lykkedes får vi en messagebox prompt der siger der er forbindelse (Ren debugging)
+                // Here we try to open the connection to the database. If we succeed we get a messagebox prompt that says there is a connection (only purpose is debugging)
                 connection.Open();
                 if (connection.State == ConnectionState.Open)
                 {
                     // Debugging
-                    //MessageBox.Show("Connection Open");
+                    // MessageBox.Show("Connection Open");
                 }
             }
-            // Hvis vores forbindelse fejler, bliver der smidt en "SQL error, samt fejl koden"
+            // If our connection fails, an "SQL error will be thrown, as well as the error code"
             catch (Exception ex)
             {
                 // Debugging
@@ -83,8 +83,11 @@ namespace IoT_Projekt
                     #region Backend Login
                     try
                     {
+                        #region Locating user credentials
                         string username = "";
                         string custid = "";
+
+                        // We want to figure out, if the information that our user types exist in our database. In order to do so, we'll search for anything under the customer table.
                         MySqlDataReader myReader = null;
                         MySqlCommand myCommand = new MySqlCommand("SELECT * FROM customers;", connection);
                         myReader = myCommand.ExecuteReader();
@@ -92,21 +95,28 @@ namespace IoT_Projekt
                         {
                             username = (string)myReader["fname"];
                             custid = (string)myReader["custid"];
+                            
+                            // if the username written by the user on the login page, matches the name in our row "fname" under the table "customers", then we'll change the CredentialOK to true.
                             if (textBoxUsername.Text.ToLower().Equals(username))
                             {
                                 CredentialOK = true;
                                 break;
                             }
                         }
-                        string customerConnection = $"Server={server};Port=3306;SslMode=none;User Id={textBoxUsername.Text.ToLower()};Password={textBoxPassword.Text};Database={database};";
+                        #endregion
 
+                        #region Opening Bank
+                        string customerConnection = $"Server={server};Port=3306;SslMode=none;User Id={textBoxUsername.Text.ToLower()};Password={textBoxPassword.Text};Database={database};";
                         connection.Close();
+                        
+                        // If the credentials are true, and our connection with the bank as AccountChecker is closed, then we want to open a new connection as the user
                         if (connection.State == ConnectionState.Closed && CredentialOK == true)
                         {
                             connection = new MySqlConnection(customerConnection);
 
                             try
                             {
+                                // If the connection succeeds, then we'll want to open a new window, i.e. our bank 
                                 connection.Open();
                                 if (connection.State == ConnectionState.Open)
                                 {
@@ -118,13 +128,18 @@ namespace IoT_Projekt
                                     #endregion
                                 }
                             }
+                            // But if the connection fails, then we'll thros an error
                             catch (Exception ex)
                             {
                                 MessageBox.Show(ex.Message, "SQL Error");
                             }
                         }
+                        #endregion
+
+                        #region No User Handling
                         else
                         {
+                            // If the user credentials typed in doesnt match anything in the database, we want to fallback to our AccountCheck user, while also telling our user that the information doesn't exist
                             string accountCheckConnection = $"Server={server};Port=3306;SslMode=none;User Id={id};Password={password};Database={database}";
                             connection = new MySqlConnection(accountCheckConnection);
                             connection.Open();
@@ -138,6 +153,7 @@ namespace IoT_Projekt
                             textBoxErrorHandeling(textBoxUsername);
                             labelUsernameMissing.Visible = true;
                         }
+                        #endregion
                     }
                     catch (Exception)
                     {
@@ -146,44 +162,49 @@ namespace IoT_Projekt
                 }
                 else
                 {
+                    // UI / UX if theres an empty password box, then it'll turn its border red
                     textBoxErrorHandeling(textBoxPassword);
                     labelPasswordMissing.Visible = true;
                 }
             }
             else
             {
+                // UI / UX if theres an empty username box, then it'll turn its border red
                 textBoxErrorHandeling(textBoxUsername);
                 labelUsernameMissing.Visible = true;
             }
             #endregion
         }
-        #region UI
+        #region UI/UX
         private void textBoxUsername_TextChanged(object sender, EventArgs e)
         {
+            // UI / UX, the username box changes border color depending on, if there is anything in the box or not, when login is pressed
             this.CreateGraphics().Clear(Login.ActiveForm.BackColor);
             labelUsernameMissing.Visible = false;
         }
 
         private void textBoxPassword_TextChanged(object sender, EventArgs e)
         {
+            // UI / UX, the password box changes border color depending on, if there is anything in the box or not, when login is pressed
             this.CreateGraphics().Clear(Login.ActiveForm.BackColor);
             labelPasswordMissing.Visible = false;
         }
 
         private void checkBoxShowPassword_CheckedChanged(object sender, EventArgs e)
         {
+            // checkbox that we can enable or disable in order to see our password
             textBoxPassword.PasswordChar = checkBoxShowPassword.Checked ? '\0' : '•';
         }
 
-
-        #endregion
-
         private void textBoxPassword_KeyUp(object sender, KeyEventArgs e)
         {
+            // When enter key is pressed, the login button is pressed
             if (e.KeyCode == Keys.Enter)
             {
                 buttonLogin.PerformClick();
             }
         }
+
+        #endregion
     }
 }
