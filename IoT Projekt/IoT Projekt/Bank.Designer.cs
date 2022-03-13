1cb1d51213c61ac6d3197ae93e04561b0ea21940
Keyvan <67221764+KeyvanSK@users.noreply.github.com>
2022-03-13 14:07:02 +0100

    Update Bank.Designer.cs

--- IoT Projekt/IoT Projekt/Bank.Designer.cs
+++ IoT Projekt/IoT Projekt/Bank.Designer.cs
@@ -33,20 +33,24 @@ namespace IoT_Projekt
             this.labelBalance = new System.Windows.Forms.Label();
             this.labelAccountName = new System.Windows.Forms.Label();
             this.panel2 = new System.Windows.Forms.Panel();
+            this.labelTransactions = new System.Windows.Forms.Label();
             this.listBoxTransactions = new System.Windows.Forms.ListBox();
             this.buttonSend = new System.Windows.Forms.Button();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.textBoxSendAmount = new System.Windows.Forms.TextBox();
             this.panel3 = new System.Windows.Forms.Panel();
-            this.labelTransactions = new System.Windows.Forms.Label();
-            this.labelMoneyTransfer = new System.Windows.Forms.Label();
-            this.labelTransferMoneyTo = new System.Windows.Forms.Label();
-            this.labelTransferAmount = new System.Windows.Forms.Label();
-            this.labelTargetMissing = new System.Windows.Forms.Label();
             this.labelAmountMissing = new System.Windows.Forms.Label();
+            this.labelTargetMissing = new System.Windows.Forms.Label();
+            this.labelTransferAmount = new System.Windows.Forms.Label();
+            this.labelTransferMoneyTo = new System.Windows.Forms.Label();
+            this.labelMoneyTransfer = new System.Windows.Forms.Label();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.buttonTakeLoan = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
             this.panel3.SuspendLayout();
+            this.panel4.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
@@ -92,6 +96,17 @@ namespace IoT_Projekt
             this.panel2.Size = new System.Drawing.Size(415, 362);
             this.panel2.TabIndex = 0;
             // 
+            // labelTransactions
+            // 
+            this.labelTransactions.AutoSize = true;
+            this.labelTransactions.Font = new System.Drawing.Font("Arial", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelTransactions.ForeColor = System.Drawing.Color.Black;
+            this.labelTransactions.Location = new System.Drawing.Point(3, 11);
+            this.labelTransactions.Name = "labelTransactions";
+            this.labelTransactions.Size = new System.Drawing.Size(161, 29);
+            this.labelTransactions.TabIndex = 2;
+            this.labelTransactions.Text = "Transactions";
+            // 
             // listBoxTransactions
             // 
             this.listBoxTransactions.FormattingEnabled = true;
@@ -102,7 +117,8 @@ namespace IoT_Projekt
             // 
             // buttonSend
             // 
-            this.buttonSend.Location = new System.Drawing.Point(22, 258);
+            this.buttonSend.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonSend.Location = new System.Drawing.Point(21, 228);
             this.buttonSend.Name = "buttonSend";
             this.buttonSend.Size = new System.Drawing.Size(247, 61);
             this.buttonSend.TabIndex = 1;
@@ -113,7 +129,7 @@ namespace IoT_Projekt
             // comboBox1
             // 
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(22, 97);
+            this.comboBox1.Location = new System.Drawing.Point(21, 91);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(247, 21);
             this.comboBox1.TabIndex = 2;
@@ -121,7 +137,7 @@ namespace IoT_Projekt
             // 
             // textBoxSendAmount
             // 
-            this.textBoxSendAmount.Location = new System.Drawing.Point(22, 181);
+            this.textBoxSendAmount.Location = new System.Drawing.Point(21, 173);
             this.textBoxSendAmount.Name = "textBoxSendAmount";
             this.textBoxSendAmount.Size = new System.Drawing.Size(247, 20);
             this.textBoxSendAmount.TabIndex = 3;
@@ -140,65 +156,8 @@ namespace IoT_Projekt
             this.panel3.Controls.Add(this.textBoxSendAmount);
             this.panel3.Location = new System.Drawing.Point(445, 101);
             this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(290, 359);
+            this.panel3.Size = new System.Drawing.Size(290, 308);
             this.panel3.TabIndex = 1;
-            // 
-            // labelTransactions
-            // 
-            this.labelTransactions.AutoSize = true;
-            this.labelTransactions.Font = new System.Drawing.Font("Arial", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelTransactions.ForeColor = System.Drawing.Color.Black;
-            this.labelTransactions.Location = new System.Drawing.Point(3, 11);
-            this.labelTransactions.Name = "labelTransactions";
-            this.labelTransactions.Size = new System.Drawing.Size(161, 29);
-            this.labelTransactions.TabIndex = 2;
-            this.labelTransactions.Text = "Transactions";
-            // 
-            // labelMoneyTransfer
-            // 
-            this.labelMoneyTransfer.AutoSize = true;
-            this.labelMoneyTransfer.Font = new System.Drawing.Font("Arial", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelMoneyTransfer.ForeColor = System.Drawing.Color.Black;
-            this.labelMoneyTransfer.Location = new System.Drawing.Point(51, 11);
-            this.labelMoneyTransfer.Name = "labelMoneyTransfer";
-            this.labelMoneyTransfer.Size = new System.Drawing.Size(191, 29);
-            this.labelMoneyTransfer.TabIndex = 3;
-            this.labelMoneyTransfer.Text = "Money Transfer";
-            // 
-            // labelTransferMoneyTo
-            // 
-            this.labelTransferMoneyTo.AutoSize = true;
-            this.labelTransferMoneyTo.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelTransferMoneyTo.ForeColor = System.Drawing.Color.Black;
-            this.labelTransferMoneyTo.Location = new System.Drawing.Point(18, 72);
-            this.labelTransferMoneyTo.Name = "labelTransferMoneyTo";
-            this.labelTransferMoneyTo.Size = new System.Drawing.Size(187, 22);
-            this.labelTransferMoneyTo.TabIndex = 4;
-            this.labelTransferMoneyTo.Text = "Transfer money to:";
-            // 
-            // labelTransferAmount
-            // 
-            this.labelTransferAmount.AutoSize = true;
-            this.labelTransferAmount.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelTransferAmount.ForeColor = System.Drawing.Color.Black;
-            this.labelTransferAmount.Location = new System.Drawing.Point(18, 156);
-            this.labelTransferAmount.Name = "labelTransferAmount";
-            this.labelTransferAmount.Size = new System.Drawing.Size(171, 22);
-            this.labelTransferAmount.TabIndex = 5;
-            this.labelTransferAmount.Text = "Transfer amount:";
-            // 
-            // labelTargetMissing
-            // 
-            this.labelTargetMissing.AutoSize = true;
-            this.labelTargetMissing.BackColor = System.Drawing.SystemColors.Control;
-            this.labelTargetMissing.Font = new System.Drawing.Font("Arial", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelTargetMissing.ForeColor = System.Drawing.Color.Red;
-            this.labelTargetMissing.Location = new System.Drawing.Point(19, 121);
-            this.labelTargetMissing.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.labelTargetMissing.Name = "labelTargetMissing";
-            this.labelTargetMissing.Size = new System.Drawing.Size(158, 14);
-            this.labelTargetMissing.TabIndex = 6;
-            this.labelTargetMissing.Text = "Please select transfer reciever!";
             // 
             // labelAmountMissing
             // 
@@ -206,12 +165,89 @@ namespace IoT_Projekt
             this.labelAmountMissing.BackColor = System.Drawing.SystemColors.Control;
             this.labelAmountMissing.Font = new System.Drawing.Font("Arial", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.labelAmountMissing.ForeColor = System.Drawing.Color.Red;
-            this.labelAmountMissing.Location = new System.Drawing.Point(19, 204);
+            this.labelAmountMissing.Location = new System.Drawing.Point(18, 196);
             this.labelAmountMissing.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.labelAmountMissing.Name = "labelAmountMissing";
             this.labelAmountMissing.Size = new System.Drawing.Size(132, 14);
             this.labelAmountMissing.TabIndex = 7;
             this.labelAmountMissing.Text = "Please enter valid amount!";
+            // 
+            // labelTargetMissing
+            // 
+            this.labelTargetMissing.AutoSize = true;
+            this.labelTargetMissing.BackColor = System.Drawing.SystemColors.Control;
+            this.labelTargetMissing.Font = new System.Drawing.Font("Arial", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelTargetMissing.ForeColor = System.Drawing.Color.Red;
+            this.labelTargetMissing.Location = new System.Drawing.Point(18, 115);
+            this.labelTargetMissing.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.labelTargetMissing.Name = "labelTargetMissing";
+            this.labelTargetMissing.Size = new System.Drawing.Size(158, 14);
+            this.labelTargetMissing.TabIndex = 6;
+            this.labelTargetMissing.Text = "Please select transfer reciever!";
+            // 
+            // labelTransferAmount
+            // 
+            this.labelTransferAmount.AutoSize = true;
+            this.labelTransferAmount.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelTransferAmount.ForeColor = System.Drawing.Color.Black;
+            this.labelTransferAmount.Location = new System.Drawing.Point(17, 148);
+            this.labelTransferAmount.Name = "labelTransferAmount";
+            this.labelTransferAmount.Size = new System.Drawing.Size(171, 22);
+            this.labelTransferAmount.TabIndex = 5;
+            this.labelTransferAmount.Text = "Transfer amount:";
+            // 
+            // labelTransferMoneyTo
+            // 
+            this.labelTransferMoneyTo.AutoSize = true;
+            this.labelTransferMoneyTo.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelTransferMoneyTo.ForeColor = System.Drawing.Color.Black;
+            this.labelTransferMoneyTo.Location = new System.Drawing.Point(17, 66);
+            this.labelTransferMoneyTo.Name = "labelTransferMoneyTo";
+            this.labelTransferMoneyTo.Size = new System.Drawing.Size(187, 22);
+            this.labelTransferMoneyTo.TabIndex = 4;
+            this.labelTransferMoneyTo.Text = "Transfer money to:";
+            // 
+            // labelMoneyTransfer
+            // 
+            this.labelMoneyTransfer.AutoSize = true;
+            this.labelMoneyTransfer.Font = new System.Drawing.Font("Arial", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelMoneyTransfer.ForeColor = System.Drawing.Color.Black;
+            this.labelMoneyTransfer.Location = new System.Drawing.Point(50, 11);
+            this.labelMoneyTransfer.Name = "labelMoneyTransfer";
+            this.labelMoneyTransfer.Size = new System.Drawing.Size(191, 29);
+            this.labelMoneyTransfer.TabIndex = 3;
+            this.labelMoneyTransfer.Text = "Money Transfer";
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.SystemColors.Control;
+            this.panel4.Controls.Add(this.label1);
+            this.panel4.Controls.Add(this.buttonTakeLoan);
+            this.panel4.Location = new System.Drawing.Point(445, 423);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(290, 40);
+            this.panel4.TabIndex = 2;
+            // 
+            // buttonTakeLoan
+            // 
+            this.buttonTakeLoan.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonTakeLoan.Location = new System.Drawing.Point(137, 8);
+            this.buttonTakeLoan.Name = "buttonTakeLoan";
+            this.buttonTakeLoan.Size = new System.Drawing.Size(145, 23);
+            this.buttonTakeLoan.TabIndex = 0;
+            this.buttonTakeLoan.Text = "1000 kr.";
+            this.buttonTakeLoan.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(17, 7);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(114, 24);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Take Loan";
             // 
             // Bank
             // 
@@ -219,10 +255,12 @@ namespace IoT_Projekt
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.RoyalBlue;
             this.ClientSize = new System.Drawing.Size(747, 475);
+            this.Controls.Add(this.panel4);
             this.Controls.Add(this.panel3);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
             this.Name = "Bank";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Fake Bank";
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Bank_FormClosed);
             this.panel1.ResumeLayout(false);
@@ -231,6 +269,8 @@ namespace IoT_Projekt
             this.panel2.PerformLayout();
             this.panel3.ResumeLayout(false);
             this.panel3.PerformLayout();
+            this.panel4.ResumeLayout(false);
+            this.panel4.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -252,5 +292,8 @@ namespace IoT_Projekt
         private System.Windows.Forms.Label labelMoneyTransfer;
         private System.Windows.Forms.Label labelAmountMissing;
         private System.Windows.Forms.Label labelTargetMissing;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonTakeLoan;
     }
 }
